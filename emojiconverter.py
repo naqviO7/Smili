99938ad9e29c75a053750b6c9a6ab957ea0e9f4c
Saqlain Naqvi <79792270+snaqvi5@users.noreply.github.com>
2021-04-28 21:48:09 +0500

    Create emojiconverter.py

---
+++ emojiconverter.py
@@ -0,0 +1,24 @@
+#simple emoji converter in python
+#you can find corresponding smily faces according to your input
+#you can get smily faces by pressing "windows+dot" in Windows Operating System
+
+#dictionary containing emojis and their smily face
+emojis={
+    ":)":"😊",
+    ":(":"😒",
+    "xD":"😂",
+    "-_-":"😑",
+    "00":"👀",
+    "*-*":"🤩",  
+    ";)":"😅",
+    ";(":"😪",
+    "^^":"😍",
+}
+
+#input from user of smily face/emoji
+emojisymbol=input("Enter Emoji 'like :)': ")
+if emojisymbol in emojis:
+    print('Smily Face corresponding to Your Emoji is:',emojis[emojisymbol])
+else:
+    print('👀 ps Sorry😕')
+    print(emojisymbol,'is not defined in Emoji Dictionary')
